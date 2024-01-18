fcb91cec131c6c89b39a3e35905b335d0d8d0d60
Guilherme Cardoso <gui.cardoso.28@gmail.com>
2024-01-18 17:42:26 -0300

    Primeiro commit e configurações iniciais do projeto

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import styles from "@/styles/homeNoAuth.module.scss"
+
+const HomeNotAuth = () => {
+  return (
+		<>
+			<Head>Teste</Head>
+			<main></main>
+		</>
+  )
+};
+
+export default HomeNotAuth;
