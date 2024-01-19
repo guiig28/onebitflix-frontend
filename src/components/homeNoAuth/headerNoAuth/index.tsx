f1ddb88d5c6aab10d5d4cb705f6c1256b977607a
Guilherme Cardoso <gui.cardoso.28@gmail.com>
2024-01-18 21:11:49 -0300

    Criação do header da pagina homeNoAuth

---
+++ src/components/homeNoAuth/headerNoAuth/index.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import { Button, Container } from "reactstrap";
+
+const HeaderNoAuth = () => {
+  return (
+    <>
+      <div className={styles.ctaSection}>
+        <img
+          src="/homeNoAuth/logoCta.png"
+          alt="logoCta"
+          className={styles.imgCta}
+        />
+        <p>Cadastre-se para ter acesso aos cursos</p>
+        <img
+          src="/homeNoAuth/logoCta.png"
+          alt="logoCta"
+          className={styles.imgCta}
+        />
+      </div>
+      <Container className={styles.nav}>
+        <img
+          src="/logoOnebitflix.svg"
+          alt="logoOnebitflix"
+          className={styles.imgLogoNav}
+        />
+        <div>
+          <Link href="/login">
+            <Button className={styles.navBtn} outline>
+              Entrar
+            </Button>
+          </Link>
+          <Link href="/login">
+            <Button className={styles.navBtn} outline>
+              Quero fazer parte
+            </Button>
+          </Link>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default HeaderNoAuth;
