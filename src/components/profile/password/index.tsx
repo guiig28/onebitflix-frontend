87dbdc54b06cd106b95f6dfd5b958fb750ed0f6b
Guilherme Cardoso <gui.cardoso.28@gmail.com>
2024-02-21 16:24:00 -0300

    Criação da estrutura do formulario de senha e do seu estilo visual

--- src/components/profile/password/index.tsx
+++ src/components/profile/password/index.tsx
@@ -1,7 +1,65 @@
 import styles from "@/styles/profile.module.scss";
+import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 
 const PasswordForm = function () {
-  return <></>;
+  return (
+    <>
+      <Form className={styles.form}>
+        <div className={styles.inputNormalDiv}>
+          <FormGroup>
+            <Label className={styles.label} for="currentPassword">
+              SENHA ATUAL
+            </Label>
+            <Input
+              name="currentPassword"
+              type="password"
+              id="currentPassword"
+              placeholder="******"
+              required
+              maxLength={12}
+              className={styles.input}
+            />
+          </FormGroup>
+        </div>
+        <div className={styles.inputFlexDiv}>
+          <FormGroup>
+            <Label className={styles.label} for="newPassword">
+              NOVA SENHA
+            </Label>
+            <Input
+              name="newPassword"
+              type="password"
+              id="newPassword"
+              placeholder="******"
+              minLength={6}
+              maxLength={12}
+              required
+              className={styles.inputFlex}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label className={styles.label} for="confirmNewPassword">
+              CONFIRMAR NOVA SENHA
+            </Label>
+            <Input
+              name="confirmNewPassword"
+              type="password"
+              id="confirmNewPassword"
+              placeholder="******"
+              minLength={6}
+              maxLength={12}
+              required
+              className={styles.inputFlex}
+            />
+          </FormGroup>
+        </div>
+
+        <Button className={styles.formBtn} outline>
+          Salvar Alterações
+        </Button>
+      </Form>
+    </>
+  );
 };
 
 export default PasswordForm;
