0a36c66ef41161e71a8013cfd075ad3984a29eef
Guilherme Cardoso <gui.cardoso.28@gmail.com>
2024-02-22 20:12:28 -0300

    Criação da página search e sua conexão com o backend através do método getSearch

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -103,6 +103,24 @@ const courseService = {
 
     return res;
   },
+
+  getSearch: async (name: string) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api
+      .get(`/courses/search?name=${name}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((err) => {
+        console.log(err.response.data.message);
+
+        return err.response;
+      });
+
+    return res;
+  },
 };
 
 export default courseService;
